2196165f842c96105175bf592fb7dfdabc9a3167
Tim Gutberlet <tim.gutberlet@insurmagic.de>
2025-03-23 14:50:04 +0000

    #1 More styling

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,10 @@
-
 import React from 'react';
 import CostCalculator from '@/components/CostCalculator';
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white to-gray-50 py-12">
-      <div className="container mx-auto px-4">
-        <div className="mb-12 text-center">
-        </div>  
-        <CostCalculator />
-      </div>
+    <div className="min-h-screen bg-white">
+      <CostCalculator />
     </div>
   );
 };
